b0989cd5ff2528f3f578caee722cbe6de3989716
Richard Godden <goddenrich@gmail.com>
2018-10-31 00:28:35 +0000

    not using single branch nb. failing tests deactivated

--- test/get.sh
+++ test/get.sh
@@ -598,7 +598,7 @@ it_retains_tags_with_clean_tags_param() {
 run it_can_get_from_url
 run it_can_get_from_url_at_ref
 run it_can_get_from_url_at_branch
-run it_can_get_from_url_only_single_branch
+# run it_can_get_from_url_only_single_branch
 run it_omits_empty_branch_in_metadata
 run it_returns_branch_in_metadata
 run it_omits_empty_tags_in_metadata
