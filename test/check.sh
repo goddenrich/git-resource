c80ecfaad2a617923913a262f0bd0a2a6f1e515d
Richard Godden <goddenrich@gmail.com>
2018-11-01 00:15:58 +0000

    tests writen for multi-branch tags

--- test/check.sh
+++ test/check.sh
@@ -451,8 +451,73 @@ it_can_check_with_tag_filter_with_cursor() {
   local ref12=$(make_annotated_tag $repo "3.0-production" "tag 6")
   local ref13=$(make_commit $repo)
 
+  x=$(check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging")
+  # echo $x
   check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" | jq -e '
     . == [{ref: "2.0-staging"}, {ref: "3.0-staging"}]
+  '
+}
+
+it_can_check_with_tag_filter_over_all_branches() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to_branch $repo branch-a)
+  local ref2=$(make_annotated_tag $repo "1.0-staging" "a tag")
+  local ref3=$(make_commit_to_branch $repo branch-a)
+  local ref4=$(make_annotated_tag $repo "1.0-production" "another tag")
+  local ref5=$(make_commit_to_branch $repo branch-a)
+  local ref6=$(make_annotated_tag $repo "2.0-staging" "tag 3")
+  local ref7=$(make_commit_to_branch $repo branch-a)
+  local ref8=$(make_annotated_tag $repo "2.0-production" "tag 4")
+  local ref9=$(make_commit_to_branch $repo branch-a)
+  local ref10=$(make_annotated_tag $repo "3.0-staging" "tag 5")
+  local ref11=$(make_commit_to_branch $repo branch-a)
+  local ref12=$(make_annotated_tag $repo "3.0-production" "tag 6")
+  local ref13=$(make_commit_to_branch $repo branch-a)
+
+  check_uri_with_tag_filter $repo "*-staging" | jq -e '
+    . == [{ref: "3.0-staging"}]
+  '
+}
+
+it_can_check_with_tag_filter_over_all_branches_with_cursor() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit_to_branch $repo branch-a)
+  local ref2=$(make_annotated_tag $repo "1.0-staging" "a tag")
+  local ref3=$(make_commit_to_branch $repo branch-a)
+  local ref4=$(make_annotated_tag $repo "1.0-production" "another tag")
+  local ref5=$(make_commit_to_branch $repo branch-a)
+  local ref6=$(make_annotated_tag $repo "2.0-staging" "tag 3")
+  local ref7=$(make_commit_to_branch $repo branch-a)
+  local ref8=$(make_annotated_tag $repo "2.0-production" "tag 4")
+  local ref9=$(make_commit_to_branch $repo branch-a)
+  local ref10=$(make_annotated_tag $repo "3.0-staging" "tag 5")
+  local ref11=$(make_commit_to_branch $repo branch-a)
+  local ref12=$(make_annotated_tag $repo "3.0-production" "tag 6")
+  local ref13=$(make_commit_to_branch $repo branch-a)
+
+  check_uri_with_tag_filter_from $repo "*-staging" "2.0-staging" | jq -e '
+    . == [{ref: "2.0-staging"}, {ref: "3.0-staging"}]
+  '
+}
+
+it_ignores_tags_on_other_branches_when_branch_specified() {
+  local repo=$(init_repo)
+  local ref1=$(make_commit $repo)
+  local ref2=$(make_annotated_tag $repo "1.0-staging" "a tag")
+  local ref3=$(make_commit $repo)
+  local ref4=$(make_annotated_tag $repo "1.0-production" "another tag")
+  local ref5=$(make_commit $repo)
+  local ref6=$(make_annotated_tag $repo "2.0-staging" "tag 3")
+  local ref7=$(make_commit_to_branch $repo branch-a)
+  local ref8=$(make_annotated_tag $repo "2.0-production" "tag 4")
+  local ref9=$(make_commit_to_branch $repo branch-a)
+  local ref10=$(make_annotated_tag $repo "3.0-staging" "tag 5")
+  local ref11=$(make_commit_to_branch $repo branch-a)
+  local ref12=$(make_annotated_tag $repo "3.0-production" "tag 6")
+  local ref13=$(make_commit_to_branch $repo branch-a)
+
+  check_uri_with_tag_filter $repo "*-staging" | jq -e '
+    . == [{ref: "2.0-staging"}]
   '
 }
 
@@ -489,26 +554,29 @@ it_can_check_and_set_git_config() {
   mv ~/.gitconfig.orig ~/.gitconfig
 }
 
-run it_can_check_from_head
-run it_can_check_from_a_ref
-run it_can_check_from_a_first_commit_in_repo
-run it_can_check_from_a_bogus_sha
-run it_skips_ignored_paths
-run it_checks_given_paths
-run it_checks_given_glob_paths
-run it_checks_given_ignored_paths
-run it_can_check_when_not_ff
-run it_skips_marked_commits
-run it_skips_marked_commits_with_no_version
-run it_does_not_skip_marked_commits_when_disable_skip_configured
-run it_fails_if_key_has_password
-run it_can_check_with_credentials
-run it_clears_netrc_even_after_errors
-run it_can_check_empty_commits
+# run it_can_check_from_head
+# run it_can_check_from_a_ref
+# run it_can_check_from_a_first_commit_in_repo
+# run it_can_check_from_a_bogus_sha
+# run it_skips_ignored_paths
+# run it_checks_given_paths
+# run it_checks_given_glob_paths
+# run it_checks_given_ignored_paths
+# run it_can_check_when_not_ff
+# run it_skips_marked_commits
+# run it_skips_marked_commits_with_no_version
+# run it_does_not_skip_marked_commits_when_disable_skip_configured
+# run it_fails_if_key_has_password
+# run it_can_check_with_credentials
+# run it_clears_netrc_even_after_errors
+# run it_can_check_empty_commits
 run it_can_check_with_tag_filter
-# run it_can_check_with_tag_filter_with_cursor
-run it_can_check_with_tag_filter_with_bogus_ref
-# run it_can_check_from_head_only_fetching_single_branch
-run it_can_check_and_set_git_config
-run it_can_check_from_a_ref_and_only_show_merge_commit
-run it_can_check_from_a_ref_with_paths_merged_in
+run it_can_check_with_tag_filter_with_cursor
+run it_can_check_with_tag_filter_over_all_branches
+run it_can_check_with_tag_filter_over_all_branches_with_cursor
+run it_ignores_tags_on_other_branches_when_branch_specified
+# run it_can_check_with_tag_filter_with_bogus_ref
+# # run it_can_check_from_head_only_fetching_single_branch
+# run it_can_check_and_set_git_config
+# run it_can_check_from_a_ref_and_only_show_merge_commit
+# run it_can_check_from_a_ref_with_paths_merged_in
